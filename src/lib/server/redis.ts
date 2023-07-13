fe22563ad4433a4a8f1a3e79093ac68e63e77401
Gertjan Assies <gertjan.assies@gmail.com>
2023-07-13 14:55:20 +0200

    asdasd

--- src/lib/server/redis.ts
+++ src/lib/server/redis.ts
@@ -1,4 +1,4 @@
 import Redis from 'ioredis';
-import { REDIS_CONNECTION } from '$env/dynamic/private';
+import { REDIS_CONNECTION } from '$env/static/private';
 
 export default REDIS_CONNECTION ? new Redis(REDIS_CONNECTION) : new Redis();

1d882e0756f31d7cc7ec9aed0670f8668d34d72c
Gertjan Assies <gertjan.assies@gmail.com>
2024-03-04 21:16:21 +0100

    small fix that svelte warned about

--- src/routes/stats/+page.server.ts
+++ src/routes/stats/+page.server.ts
@@ -2,5 +2,5 @@ import type { PageServerLoad } from './$types';
 import { getClient, getPageStats} from '$lib/server/redis.ts';
 
 export const load: PageServerLoad = (async () => {
-  return { pageviews: getPageStats(await getClient()) };
+  return { pageviews: await getPageStats(await getClient()) };
 })
